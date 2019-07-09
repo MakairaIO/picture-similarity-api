2ccf6cef9afecea9730e62f929d6128176abe352
Florian Ludwig <ludwig@marmalade.de>
2019-07-09 15:11:18 +0200

    Adds fixture bundle and pingdom healthcheck fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PictureSimilarity;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $pictureSimPingDom = new PictureSimilarity();
+        $pictureSimPingDom->setProductId('testproduct');
+        $pictureSimPingDom->setSimilarIds(['its', 'the', 'pingdom', 'test']);
+        $pictureSimPingDom->setShop('testshop');
+        $pictureSimPingDom->setUpdatedAt(new \DateTime('2999-01-01 00:00:00'));
+
+        $manager->persist($pictureSimPingDom);
 
         $manager->flush();
     }
