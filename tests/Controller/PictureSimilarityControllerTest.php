9ec7f100a7d4be3c77c00f496b3478aeb4a3ee54
Sunny <dt@marmalade.group>
2021-01-19 11:20:15 +0700

    [MAKAIRA-1377] Improve unit tests

--- tests/Controller/PictureSimilarityControllerTest.php
+++ tests/Controller/PictureSimilarityControllerTest.php
@@ -9,44 +9,51 @@ class PictureSimilarityControllerTest extends WebTestCase
     public function testFind()
     {
         $client = static::createClient();
-        $expectedResponse = [
-            "its",
-            "the",
-            "pingdom",
-            "test"
-        ];
 
         // Test without type
-        $client->request('GET', '/api/testshop/testproduct');
+        $client->request('GET', '/api/testshop/1');
         $actualResponse = json_decode($client->getResponse()->getContent(), true);
+        $expectedResponse = [
+            "image",
+            "type",
+            "product",
+            "1"
+        ];
         $this->assertEquals($expectedResponse, $actualResponse);
 
         // Test with type
-        $client->request('GET', '/api/image/testshop/testproduct');
+        $client->request('GET', '/api/notImage/testshop/1');
         $actualResponse = json_decode($client->getResponse()->getContent(), true);
+        $expectedResponse = [
+            "notImage",
+            "type",
+            "product",
+            "1"
+        ];
         $this->assertEquals($expectedResponse, $actualResponse);
     }
 
     public function testFindByProductIds()
     {
         $client = static::createClient();
-        $expectedResponse = [
-            [
-                "its",
-                "the",
-                "pingdom",
-                "test"
-            ]
-        ];
 
         // Test without type
-        $client->request('GET', '/api/testshop/products/testproduct');
+        $client->request('GET', '/api/testshop/products/1,2,3');
         $actualResponse = json_decode($client->getResponse()->getContent(), true);
+        $expectedResponse = [
+            ["image", "type", "product", "1"],
+            ["image", "type", "product", "2"],
+            ["image", "type", "product", "3"]
+        ];
         $this->assertEquals($expectedResponse, $actualResponse);
 
         // Test with type
-        $client->request('GET', '/api/image/testshop/products/testproduct');
+        $client->request('GET', '/api/notImage/testshop/products/4,5');
         $actualResponse = json_decode($client->getResponse()->getContent(), true);
+        $expectedResponse = [
+            ["notImage", "type", "product", "4"],
+            ["notImage", "type", "product", "5"],
+        ];
         $this->assertEquals($expectedResponse, $actualResponse);
     }
 }
