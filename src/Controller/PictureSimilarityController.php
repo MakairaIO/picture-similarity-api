63d6c317177bccaf664e1d80f68a1dff75241ef0
Sunny <dt@marmalade.group>
2021-01-19 10:43:36 +0700

    [MAKAIRA-1377] Created new functions and unit test

--- src/Controller/PictureSimilarityController.php
+++ src/Controller/PictureSimilarityController.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\PictureSimilarity;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class PictureSimilarityController extends Controller
+class PictureSimilarityController extends AbstractController
 {
     /**
      * Find similar products by shop and productId
      *
+     * @param $shop
+     * @param $productId
+     * @param string $type
      * @return Response
      */
-    public function find($shop, $productId, $type = 'image')
+    public function find($shop, $productId, $type = 'image'): Response
     {
         $type = $type === 'image' ? array('image', '') : $type;
 
@@ -23,5 +27,27 @@ class PictureSimilarityController extends Controller
         }
 
         return $this->json($similar[0]);
+    }
+
+    /**
+     * Find similar products by shop and productIds
+     *
+     * @param $productIds
+     * @param $shop
+     * @param string $type
+     * @return Response
+     */
+    public function findByProductIds($shop, $productIds, $type = 'image'): Response
+    {
+        $type = $type === 'image' ? array('image', '') : $type;
+        $productIds = explode(',', $productIds);
+
+        $repository = $this->getDoctrine()->getRepository(PictureSimilarity::class);
+        $similar = $repository->findBy(['productId' => $productIds, 'shop' => $shop, 'type' => $type], ['updatedAt' => 'DESC']);
+        if (!$similar) {
+            throw $this->createNotFoundException('No Similar Products found.');
+        }
+
+        return $this->json($similar);
     }
 }
