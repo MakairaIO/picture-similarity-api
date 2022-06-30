7b40c9fc69e70c872bd1f03f937654e9342f58db
Stefan Krenz <sk@marmalade.group>
2022-06-30 15:41:10 +0200

    Add migration

---
+++ src/Migrations/Version20220630140043.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220630140043 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE picture_similarity ADD INDEX updated_at (updated_at)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE picture_similarity DROP INDEX updated_at (updated_at)');
+    }
+}
