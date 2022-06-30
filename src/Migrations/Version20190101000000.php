747e50b0bed012343bd7ecd9bb65c182803548f4
Stefan Krenz <sk@marmalade.group>
2022-06-30 16:15:30 +0200

    Fix migrations

--- src/Migrations/Version20190101000000.php
+++ src/Migrations/Version20190101000000.php
@@ -12,12 +12,11 @@ class Version20190101000000 extends AbstractMigration
         $this->addSql(
             'CREATE TABLE picture_similarity (
                 id INT AUTO_INCREMENT NOT NULL,
-                product_id VARCHAR(255) NOT NULL,
-                similar_ids JSON NOT NULL,
-                shop VARCHAR(255) NOT NULL,
+                product_id VARCHAR(100) NOT NULL COLLATE utf8mb4_unicode_ci,
+                similar_ids LONGTEXT NOT NULL COLLATE utf8mb4_bin,
+                shop VARCHAR(100) NOT NULL COLLATE utf8mb4_unicode_ci,
                 updated_at DATETIME NOT NULL,
-                type VARCHAR(255) NOT NULL,
-                INDEX search_idx (product_id, shop, type),
+                INDEX search_idx (product_id, shop),
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;'
         );
